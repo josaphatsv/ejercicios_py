553419d5fd60b8abca6080f3fe739009141d9fe2
Josaphat <josa1980@gmail.com>
2021-06-04 14:35:54 -0600

    Literales o Variables

---
+++ Leccion2/main.py
@@ -0,0 +1,12 @@
+"""Leccion variables"""
+miVariable= "Hola desde Python"
+print(miVariable)
+print(miVariable)
+print(miVariable)
+"""Renombra una variable"""
+miVariable=2
+print(miVariable)
+print(miVariable)
+"""Renombrar la misma variable"""
+miVariable=10
+print(miVariable)
