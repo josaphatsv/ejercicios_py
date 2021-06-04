cb45b3a121e3eedad999d0048b209824b2a69174
Josaphat <josa1980@gmail.com>
2021-06-04 15:47:25 -0600

    entrada del usuario
    forma de capturar informacion desde el teclado

---
+++ Leccion6/main.py
@@ -0,0 +1,7 @@
+"""Entrada del usuario """
+nombre=input("Como te llamas ? ")
+print(" Hola ",nombre," es un gusto conocerte" )
+
+numero1=input("Brindame un numero : ")
+numero2=input("Brindame otro numero : ")
+print("La suma de los dos numeros es ", int(numero1)+int(numero2))
