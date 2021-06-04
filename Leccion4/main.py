8591fb3e95371e096131801c8af4fbb918d2cfbc
Josaphat <josa1980@gmail.com>
2021-06-04 14:37:01 -0600

    saber el espacio de memoria de una variable

---
+++ Leccion4/main.py
@@ -0,0 +1,9 @@
+"""Una literal es un valor constante formado por una secuencia de caracteres. """
+x=5
+y=10
+z=x+y
+print(x)
+print(y)
+print(z)
+"""ocuparemos id para saber el espacio en memoria de una Literal o variable """
+print(id(z))
