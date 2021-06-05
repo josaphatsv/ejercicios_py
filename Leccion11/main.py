deaae399f14ea2b539b214b397a870c2d3c7d4e6
Josaphat <josa1980@gmail.com>
2021-06-04 20:32:04 -0600

    Ejercicio Etapas de la vida segun la edad

---
+++ Leccion11/main.py
@@ -0,0 +1,10 @@
+edad=int( input( "Proporcione la edad actual : " ) )
+
+if edad>=0 and edad<10:
+    print( 'La infacia es increible...' )
+elif edad>=10 and edad<20:
+    print( "Muchos cambios y mucho estudio" )
+elif edad>=20 and edad<=30:
+    print( "Amor y comienza el trabajo..." )
+else:
+    print( "Etapa de la vida no Reconocida")
