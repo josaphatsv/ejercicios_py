d92bc24edd0d5cfd845bba2bc8f075cbea83d9f2
Josaphat <josa1980@gmail.com>
2021-06-04 20:06:44 -0600

    Operadores Matematicos

---
+++ Leccion8/main.py
@@ -0,0 +1,19 @@
+"""
+Operadores matematicos
+"""
+numero1=3
+numero2=2
+suma=numero1+numero2
+print('Resultado de la suma es :',suma)
+#Uso de interpolacion
+print(f'Resultado de la suma {numero1} y {numero2} es {suma}')
+resta=numero1-numero2
+print(f'Resultado de la resta de {numero1} menos {numero2} es {resta}')
+multiplicacion=numero1*numero2
+print(f'Resultado de la multiplicacion de {numero1} por {numero2} es {multiplicacion}')
+division=numero1/numero2
+print(f'Resultado de la division de {numero1} entre {numero2} es {division}')
+otradivision=numero1//numero2
+print(f"Resultado de la division de {numero1} entre {numero2} es {otradivision}")
+modulo=numero1%numero2
+print(f"Residuo de la division de {numero1} y {numero2} es {modulo}")
